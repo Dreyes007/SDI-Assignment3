e837cdf0753cebf198a6bc9e27b43bc42e2f560d
Daniel Reyes <Kurlzs@Daniels-MacBook-Pro.local>
2012-09-13 00:28:11 -0400

    Added Procedure Method

--- Reyes_daniel_assignment3.js
+++ Reyes_daniel_assignment3.js
@@ -5,7 +5,27 @@
 //Global Variables
 var horseName = "Crazy Horse"
 var streetNames = ["Calle 4", "Fast Ave", "Hurt Cir"],
-var equipment = ["Saddle", "Spurs", "Harness"],
+speedOfHorse = [10, 15, 20],
+equipment = ["Saddle", "Spurs", "Harness"];
 var numberOfEquipment = 3;
 var nuts = true;
+var miles = 10;
 
+//Method: Procedure
+
+var riddenMiles = function(name, message, m) {
+	var howFar = m + m;
+	
+		if (nuts === true) {
+		rider = (name + " is crazy!, " + message + m + " miles ");
+	} else {
+		console.log(name + " is a great rider ");
+		
+	};
+		return rider;
+	
+};
+
+//Main code
+var rideCall = riddenMiles("Pedro","riding this horse for ", 10);
+console.log(rideCall)

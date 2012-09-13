73d58f068b3f595ad71f14f5993648a825ae5fb3
Daniel Reyes <Kurlzs@Daniels-MacBook-Pro.local>
2012-09-12 20:22:03 -0400

    Added Global variables

--- Reyes_daniel_assignment3.js
+++ Reyes_daniel_assignment3.js
@@ -2,3 +2,10 @@
    SDI - 1209
    Project 3
 */
+//Global Variables
+var horseName = "Crazy Horse"
+var streetNames = ["Calle 4", "Fast Ave", "Hurt Cir"],
+var equipment = ["Saddle", "Spurs", "Harness"],
+var numberOfEquipment = 3;
+var nuts = true;
+
